8fd77aed30b5598ae3ba723cd3f6bf4d5778077d
frog <hello@frogtools.xyz>
2023-08-11 16:19:47 -0400

    Added Support For Discord Messages from, and updated Secrets

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,14 +1,51 @@
 import AnyCase from "./AnyCase";
-
 import { getCommands, helpCommandResponse, Command, writeBotFatherCommandsList, defaultCommands } from "@commands";
+import { WebhookClient } from "discord.js";
 import { Telegraf } from "telegraf";
+import { channelPost } from "telegraf/filters";
 
 const App = async () => {
-  const { BOT_TOKEN = "" } = process.env;
+  const { BOT_TOKEN = "", ALLOWED_CHANNEL_ID = 0, DISCORD_WEBHOOK = "" } = process.env;
   const bot = new Telegraf(BOT_TOKEN);
   AnyCase.apply(bot);
   const mirrorCommands = await getCommands(false);
   const commands = [...mirrorCommands, ...defaultCommands];
+
+  bot.on(channelPost("photo"), async (ctx) => {
+    if (ctx.update.channel_post.chat.id === Number(ALLOWED_CHANNEL_ID)) {
+      const fileId = ctx.update.channel_post.photo[0].file_id;
+      const fileLink = await ctx.telegram.getFileLink(fileId);
+
+      const webhookClient = new WebhookClient({ url: DISCORD_WEBHOOK });
+
+      webhookClient
+        .send({ content: ctx.channelPost.caption, files: [{ attachment: fileLink.href }] })
+        .then(() => {
+          console.log("Message sent successfully");
+          webhookClient.destroy(); // Don't forget to destroy the client
+        })
+        .catch((error) => {
+          console.error("Error sending message:", error);
+          webhookClient.destroy(); // In case of error, destroy the client
+        });
+    }
+  });
+
+  bot.on(channelPost("text"), (ctx) => {
+    if (ctx.update.channel_post.chat.id === Number(ALLOWED_CHANNEL_ID)) {
+      const webhookClient = new WebhookClient({ url: DISCORD_WEBHOOK });
+      webhookClient
+        .send(ctx.channelPost.text)
+        .then(() => {
+          console.log("Message sent successfully");
+          webhookClient.destroy(); // Don't forget to destroy the client
+        })
+        .catch((error) => {
+          console.error("Error sending message:", error);
+          webhookClient.destroy(); // In case of error, destroy the client
+        });
+    }
+  });
 
   commands.forEach((command: Command) => {
     bot.command(command.alias, (ctx) => ctx.reply(command.response));
