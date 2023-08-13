5284029c5d1908b0c56d4b1055ec465585c6868f
frog <hello@frogtools.xyz>
2023-08-13 17:49:28 -0400

    update onRelease.yaml and src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,11 @@ app.use(express.json());
 app.post("/snapshot", (req, res) => {
   console.log(req.body);
   console.log(req.headers);
-  return res.send("Received a POST HTTP method");
+  if (req.headers.authentication === process.env.SNAPSHOT_WEBHOOK_SECRET) {
+    return res.send("Received a POST HTTP method");
+  } else {
+    return res.send("Failed to authenticate.");
+  }
 });
 
 app.listen(8080, () => console.log(`Server listening on port 8080!`));
