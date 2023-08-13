398a213bd1e36e511803d4954ec53ad15adeb2f5
frog <hello@frogtools.xyz>
2023-08-13 17:10:16 -0400

    build(deps): update package-lock.json, package.json and src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import bot from "@bot";
 import * as e from "dotenv";
+import express from "express";
 
 e.config();
 bot();
+const app = express();
+
+app.post("/snapshot", (req, res) => {
+  console.log(req.body);
+  return res.send("Received a POST HTTP method");
+});
+
+app.listen(8080, () => console.log(`Server listening on port 8080!`));
